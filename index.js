087456e63c577024de8e1667c51c4e643d4e594b
Nick Matantsev <nick.matantsev@gmail.com>
2017-07-12 02:06:22 -0400

    Revert "tweak test code to make sure it works in hardware TTY"
    This reverts commit 6d8117c1d8107c4b58cb8bded5fb36bc89db0b9e.

--- index.js
+++ index.js
@@ -33,79 +33,42 @@ const render = require('react-blessed').render;
 //   }
 // }
 
-// const screen = blessed.screen({
-//     autoPadding: true,
-//     smartCSR: true,
-//     terminal: 'vt220',
-//     input: fs.createReadStream('/dev/ttyUSB0'),
-//     output: fs.createWriteStream('/dev/ttyUSB0'),
-//     title: 'Twitty'
-// });
-
-// // Create a box perfectly centered horizontally and vertically.
-// var box = blessed.box({
-//   top: 'center',
-//   left: 'center',
-//   width: '50%',
-//   height: '50%',
-//   content: 'Hello {bold}world{/bold}!',
-//   tags: true,
-//   border: {
-//     type: 'line'
-//   },
-//   // style: {
-//   //   fg: 'white',
-//   //   bg: 'magenta',
-//   //   border: {
-//   //     fg: '#f0f0f0'
-//   //   },
-//   //   hover: {
-//   //     bg: 'green'
-//   //   }
-//   // }
-// });
-
-// // Append our box to the screen.
-// screen.append(box);
-
-// screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-//     return process.exit(0);
-// });
-
-// screen.render();
-
-const program = blessed.program({
+const screen = blessed.screen({
+    autoPadding: true,
+    smartCSR: true,
     input: fs.createReadStream('/dev/ttyUSB0'),
     output: fs.createWriteStream('/dev/ttyUSB0'),
+    title: 'Twitty'
 });
 
-program.key('q', function(ch, key) {
-  program.clear();
-  program.disableMouse();
-  program.showCursor();
-  program.normalBuffer();
-  process.exit(0);
+// Create a box perfectly centered horizontally and vertically.
+var box = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: '50%',
+  height: '50%',
+  content: 'Hello {bold}world{/bold}!',
+  tags: true,
+  border: {
+    type: 'line'
+  },
+  // style: {
+  //   fg: 'white',
+  //   bg: 'magenta',
+  //   border: {
+  //     fg: '#f0f0f0'
+  //   },
+  //   hover: {
+  //     bg: 'green'
+  //   }
+  // }
 });
 
-program.on('mouse', function(data) {
-  if (data.action === 'mousemove') {
-    program.move(data.x, data.y);
-    program.bg('red');
-    program.write('x');
-    program.bg('!red');
-  }
+// Append our box to the screen.
+screen.append(box);
+
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+    return process.exit(0);
 });
 
-program.alternateBuffer();
-program.enableMouse();
-program.hideCursor();
-program.clear();
-
-program.move(1, 1);
-program.bg('black');
-program.write('Hello world', 'blue fg');
-program.setx((program.cols / 2 | 0) - 4);
-program.down(5);
-program.write('Hi again!');
-program.bg('!black');
-program.feed();
+screen.render();
