37b16b3e12b1161b31bb069374313d45c14fa1af
Nick Matantsev <nick.matantsev@gmail.com>
2017-07-13 01:34:54 -0400

    Use React to render Blessed content

---
+++ App.js
@@ -0,0 +1,20 @@
+const React = require('react');
+
+// Rendering a simple centered box
+class App extends React.Component {
+    render() {
+        return React.createElement(
+            'box',
+            {
+                top: "center",
+                left: "center",
+                width: "50%",
+                height: "50%",
+                border: { type: 'line' }
+            },
+            'Hello Reactive World!'
+        );
+    }
+}
+
+module.exports = App;
--- index.js
+++ index.js
@@ -4,21 +4,7 @@ const React = require('react');
 const blessed = require('blessed');
 const render = require('react-blessed').render;
 
-// Rendering a simple centered box
-// class App extends Component {
-//   render() {
-//     return (
-//       <box top="center"
-//            left="center"
-//            width="50%"
-//            height="50%"
-//            border={{type: 'line'}}
-//            style={{border: {fg: 'blue'}}}>
-//         Hello World!
-//       </box>
-//     );
-//   }
-// }
+const App = require('./App');
 
 const inputStream = new tty.ReadStream(fs.openSync('/dev/ttyUSB0', 'r'));
 const outputStream = new tty.WriteStream(fs.openSync('/dev/ttyUSB0', 'w'));
@@ -36,24 +22,8 @@ const screen = blessed.screen({
     title: 'Twitty'
 });
 
-// Create a box perfectly centered horizontally and vertically.
-var box = blessed.box({
-  top: 'center',
-  left: 'center',
-  width: '50%',
-  height: '50%',
-  content: 'Hello {bold}world{/bold}!',
-  tags: true,
-  border: {
-    type: 'line'
-  }
-});
-
-// Append our box to the screen.
-screen.append(box);
-
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
     return process.exit(0);
 });
 
-screen.render();
+render(React.createElement(App), screen);
