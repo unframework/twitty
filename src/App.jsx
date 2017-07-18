883ff2f1e3aee414d8438b2b98fc049051b6aa11
Nick Matantsev <nick.matantsev@gmail.com>
2017-07-18 02:30:02 -0400

    Add simple RSS headlines fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,76 @@
 
 const React = require('react');
 const blessed = require('blessed');
+const FeedParser = require('feedparser');
+const request = require('request');
 
-// Rendering a simple centered box
+function getRSS(url) {
+    return new Promise((resolve, reject) => {
+        const feedparser = new FeedParser({});
+        const result = [];
+
+        feedparser.on('end', () => {
+            resolve(result);
+        });
+
+        feedparser.on('readable', function () {
+            var item;
+
+            while (item = feedparser.read()) {
+                result.push(item);
+            }
+        });
+
+        const req = request(url);
+
+        req.on('response', function (res) {
+            if (res.statusCode !== 200) {
+                reject(new Error('error fetching data'));
+            } else {
+                req.pipe(feedparser);
+            }
+        });
+    });
+}
+
 class App extends React.Component {
     constructor(props) {
         super();
 
         this.state = {
-            on: false
+            headlines: null,
+            fetchRequest: getRSS('http://www.ctvnews.ca/rss/ctvnews-ca-sci-tech-public-rss-1.822295')
         };
     }
+
     componentDidMount() {
-        this._blinkerId = setInterval(() => {
-            this.setState((state) => ({ on: !state.on }));
-        }, 400);
+        this.state.fetchRequest.then((rssEntries) => {
+            this.setState({
+                headlines: rssEntries.map(entry => entry.title),
+                fetchRequest: null
+            });
+        });
     }
 
     render() {
         return <box
-            top="center"
-            left="center"
-            width="50%"
-            height="50%"
-            border={{ type: 'line' }}
+            top="0"
+            left="0"
+            right="0"
+            bottom="0"
         >
-            {blessed.parseTags(
-                this.state.on
-                    ? 'Hello {bold}Reactive{/bold} World!'
-                    : 'Hello Reactive World!'
-            )}
+            <box
+                top="0"
+                left="0"
+                width="50%"
+                height="50%"
+                border={{ type: 'line' }}
+            >
+                {this.state.fetchRequest
+                    ? 'Loading...'
+                    : this.state.headlines.join("\n")
+                }
+            </box>
         </box>;
     }
 }
